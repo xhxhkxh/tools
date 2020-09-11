de75ec5e7310653badc13430733e253c9cacdaf3
xhxhkxh <45535500+xhxhkxh@users.noreply.github.com>
2020-09-11 22:12:26 +0800

    Add File Hider.exe
    这里有所有的源代码，和visual studio解决项目。

---
+++ 隐写/隐写/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace 隐写
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "需要隐藏的文件";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "配套文件";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "打开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "制作";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "输出文件夹";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "位置";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "位置";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "文件名";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(307, 121);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(192, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "制作完成后打开文件所在文件夹";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(306, 141);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(168, 16);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "制作完成后删除隐藏原文件";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(306, 163);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(132, 16);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "制作完成后关闭程序";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "目前正在处理：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(103, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "空闲";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "帮助文档";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button3;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 194);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(527, 233);
+            this.MinimumSize = new System.Drawing.Size(527, 233);
+            this.Name = "Form1";
+            this.Text = "FileHider";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 隐写/隐写/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace 隐写
+{
+    public partial class Form1 : Form
+    {
+        public string str;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            label8.Text = "打开文件......";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                label4.Text = open.FileName;
+            }
+            label8.Text = "空闲";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            label8.Text = "打开文件......";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                label5.Text = open.FileName;
+                label3.Text = System.IO.Path.GetDirectoryName(label5.Text);
+            }
+            label8.Text = "空闲";
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            string Fname = System.IO.Path.GetFileName(label5.Text);
+            if (label4.Text == "位置" || label5.Text == "位置")
+            {
+                MessageBox.Show("请选择正确的文件", "出现毁灭性错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string str = "copy /b " + "\"" + label4.Text + " \" + \"" + label5.Text + "\" \"" + label5.Text + "\"";
+                label8.Text = "制作中......";
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(str);
+                p.StandardInput.WriteLine("Yes");
+                p.StandardInput.WriteLine("exit");
+                p.StandardInput.Close();
+                // string output = p.StandardOutput.ReadToEnd();
+                // MessageBox.Show(output, "调试信息");
+                MessageBox.Show("操作成功", "", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                label8.Text = "空闲";
+                p.Close();
+            }
+            if (checkBox1.Checked == true)
+            {
+                label8.Text = "打开文件夹";
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine("start "+label3.Text);
+                p.StandardInput.Close();
+                p.WaitForExit();
+                p.Close();
+                label8.Text = "空闲";
+            }
+            if(checkBox2.Checked == true)
+            {
+                label8.Text = "删除源文件";
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.Start();
+                p.StandardInput.WriteLine("del " + label4.Text);
+                p.StandardInput.Close();
+                label8.Text = "空闲";
+            }
+            if (checkBox3.Checked == true)
+            {
+                label8.Text = "关闭程序......";
+                System.Environment.Exit(0);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string Npath = System.AppDomain.CurrentDomain.BaseDirectory;
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+            p.StandardInput.WriteLine("cd " + Npath);
+            p.StandardInput.WriteLine(Npath[0] + ":");
+            p.StandardInput.WriteLine("start ./README.txt");
+            p.StandardInput.Close();
+            string Out = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+        }
+
+
+    }
+}
